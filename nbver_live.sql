fbaa3c392a9690dcb21748addfa395be98c369a0
ryanschen <asy99012@163.com>
2017-11-19 22:05:55 +0800

    edit

---
+++ nbver_live.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.7.17 : Database - nbver_live
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`nbver_live` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `nbver_live`;
+
+/*Table structure for table `goods` */
+
+DROP TABLE IF EXISTS `goods`;
+
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `price` float(10,2) NOT NULL,
+  `creat_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+
+/*Data for the table `goods` */
+
+insert  into `goods`(`id`,`name`,`price`,`creat_time`) values (1,'apple',5.00,'2017-10-04 20:12:13'),(2,'banner',5.00,'2017-05-11 01:37:37'),(25,'hehe',12.00,'2017-10-04 18:40:49'),(26,'jo233',12.00,'2017-10-04 20:10:47'),(27,'jj呵呵23234',222.00,'2017-10-04 21:37:48'),(28,'rr44',23.00,'2017-10-04 22:14:59'),(30,'wer',123.00,'2017-11-19 21:27:50');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
